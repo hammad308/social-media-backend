ddbed37e92393df950a0c07691cf8d321a000030
Muhammad Hammad Ali <mh6139251@gmail.com>
2026-06-22 16:52:12 +0500

    feat: add configuration files for Cloudinary, MongoDB, and Passport authentication

---
+++ src/config/passport.js
@@ -0,0 +1,39 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../modules/user/userModel");
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "/api/auth/google/callback"
+        },
+        async (
+            accessToken,
+            refreshToken,
+            profile,
+            done
+        ) => {
+            try {
+                const email = profile.emails[0].value;
+                let user = await User.findOne({
+                    email
+                });
+                if (!user) {
+                    user = await User.create({
+                        username: profile.displayName,
+                        email,
+                        profilePicture: profile.photos?.[0]?.value,
+                        googleId: profile.id
+                    });
+                }
+                done(null, user);
+            }
+            catch (error) {
+                done(error, null);
+            }
+        }
+    )
+);
+module.exports = passport;
